5bc0c56e9b7da6fd9e523e8415ac7d640067f569
joel <joelbabs1992@gmail.com>
2023-08-25 14:35:03 -0700

    update

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * main - inpunt point
- * @ac: arg count
- * @av: arg vector
+ * main -the  entry point
+ * @ac: arg count.
+ * @av: arg vector.
  *
  * Return: 0 if success, 1 if error
  */
@@ -13,9 +13,9 @@ int main(int ac, char **av)
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (fd)
-			: "r" (fd));
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
 	if (ac == 2)
 	{
